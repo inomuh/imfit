7de7fb9e3852306f867d99a79c84bd727216df08
Cem Bağlum <39911281+cembglm@users.noreply.github.com>
2022-08-10 13:02:58 +0300

    Add files via upload

---
+++ test/Test 03/Mutant_no_43_T3_LCPN_3-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(s: str) -> int:
+        charSet *=*= set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+print(Solution.lengthOfLongestSubstring(s = "pwwkew"))
